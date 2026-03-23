0fc078e1e72171546283cfb5206c4cdea2f8675e
mohamedachem-droid <mohamed.achem@gmail.com>
2026-03-23 12:25:56 +0100

    Fix command syntax for removing directory

---
+++ script.sh
@@ -0,0 +1,15 @@
+momo_@pc_de_mohamed MINGW64 ~
+$ mkdir temp
+mkdir: cannot create directory ‘temp’: File exists
+
+momo_@pc_de_mohamed MINGW64 ~
+$ rm-r temp
+bash: rm-r: command not found
+
+
+momo_@pc_de_mohamed MINGW64 ~
+$ mkdir temp
+mkdir: cannot create directory ‘temp’: File exists
+
+momo_@pc_de_mohamed MINGW64 ~
+$ rm-r temp
